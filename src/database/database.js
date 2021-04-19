de1bb62807e07006d689d7f5c886126512ae8676
Janio Samuel <samucagomes08@gmail.com>
2021-04-19 19:59:04 -0300

    Added env variables

--- src/database/database.js
+++ src/database/database.js
@@ -1,4 +1,6 @@
 const Sequelize = require('sequelize');
-const sequelize = new Sequelize('chat', 'root', 'admin', { dialect: 'postgres', host: 'localhost' });
+require('dotenv-safe').config();
+
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, { dialect: 'postgres', host: process.env.DB_URL, port: process.env.DB_PORT });
 
 module.exports = sequelize;
